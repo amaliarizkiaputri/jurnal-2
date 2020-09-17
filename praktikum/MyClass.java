389f14a0d3bb2da67efbb97a103f02dd5618b3ec
unknown <amaliarizkiaputri93@gmail.com>
2020-09-17 20:48:55 +0800

    MyClass.java

---
+++ praktikum/MyClass.java
@@ -0,0 +1,8 @@
+public class MyClass {
+
+     public static void main (String[] args) {
+	     System.out.println("nama:amalia rizkia putri");
+	     System.out.println("nim:6706204127");
+	     System.out.println("kelas:D3RPLA4404");
+	} 
+}
